56c89ce31a6e6dc06a8b28ac65f955d4d8cb109b
Soph <111960629+sophiaacohon@users.noreply.github.com>
2024-10-18 22:30:45 +0800

    Add files via upload

---
+++ e1_module1.js
@@ -0,0 +1,23 @@
+//question 1
+console.log("Sophia Cohon");
+
+//question 2
+console.log("2002");
+
+//question 3
+console.log(2002);
+
+//question 4
+console.log("Sophia Cohon (2002)");
+console.log("Sophia", "Cohon", "(2002)");
+
+//question 5
+console.log("Sophia");
+console.log("Cohon");
+console.log("(2002)");
+
+//question 6
+console.log("Sophia" + "Cohon" + "(2002)");
+
+//question 7
+console.log("Sophia " + "Cohon " + "(2002)");
