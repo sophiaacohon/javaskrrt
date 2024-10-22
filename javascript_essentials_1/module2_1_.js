e758b95b02d9fcbefdf8c1ecc149e7469065440a
Soph <111960629+sophiaacohon@users.noreply.github.com>
2024-10-22 14:24:14 +0800

    finished module 2.1

---
+++ javascript_essentials_1/module2_1_.js
@@ -0,0 +1,46 @@
+/*
+//typeof
+let year = 1990;
+console.log(typeof year); // -> number
+console.log(typeof 1991); // -> number
+   
+let name = "Alice";
+console.log(typeof name); // -> string
+console.log(typeof "Bob"); // -> string
+   
+let typeOfYear = typeof year;
+console.log(typeOfYear); // -> number
+console.log(typeof typeOfYear); // -> string
+*/
+
+/*
+//boolean
+let  isDataValid  =  true;
+let  isStringTooLong  =  false;
+let  isGameOver  =  false;
+continueLoop  =  true;
+   
+console.log(false);  //  ->  false
+console.log(typeof  false);  //  ->  boolean
+console.log(isDataValid);  //  ->  true
+console.log(typeof  isDataValid);  //  ->  boolean
+*/
+
+/*
+//string functions
+let  str  =  "java script language";
+   
+console.log(str.length);  //  ->  20
+console.log('test'.length);  //  ->  4
+   
+console.log(str.charAt(0));  //  ->  'j'
+console.log('abc'.charAt(1));  //  ->  'b'
+   
+console.log(str.slice(0,  4));  //  ->  'java'
+console.log('test'.slice(1,  3));  //  ->  'es'
+   
+console.log(str.split('  '));  //  ->  ['java',  'script',  'language']
+console.log('192.168.1.1'.split('.'));    //  ->  ['192',  '168',  '1',  '1']
+*/
+
+//
