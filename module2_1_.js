b0658c38ee00ac6459931bcb0a117605bdbb0133
Soph <111960629+sophiaacohon@users.noreply.github.com>
2024-10-23 16:26:37 +0800

    done with module 2

---
+++ module2_1_.js
@@ -0,0 +1,66 @@
+/*
+//typeof
+let year = 1990;
+console.log(typeof year); // -> number
+console.log(typeof 1991); // -> number
+   
+let name = "Alice";
+console.log(typeof name); // -> string
+console.log(typeof "Bob"); // -> string
+   
+let typeOfYear = typeof year;
+console.log(typeOfYear); // -> number
+console.log(typeof typeOfYear); // -> string
+*/
+
+/*
+//boolean
+let  isDataValid  =  true;
+let  isStringTooLong  =  false;
+let  isGameOver  =  false;
+continueLoop  =  true;
+   
+console.log(false);  //  ->  false
+console.log(typeof  false);  //  ->  boolean
+console.log(isDataValid);  //  ->  true
+console.log(typeof  isDataValid);  //  ->  boolean
+*/
+
+/*
+//string functions
+let  str  =  "java script language";
+   
+console.log(str.length);  //  ->  20
+console.log('test'.length);  //  ->  4
+   
+console.log(str.charAt(0));  //  ->  'j'
+console.log('abc'.charAt(1));  //  ->  'b'
+   
+console.log(str.slice(0,  4));  //  ->  'java'
+console.log('test'.slice(1,  3));  //  ->  'es'
+   
+console.log(str.split('  '));  //  ->  ['java',  'script',  'language']
+console.log('192.168.1.1'.split('.'));    //  ->  ['192',  '168',  '1',  '1']
+*/
+
+//BigInt
+let  big  =  1234567890000000000000n;
+let  big2  =  1n;
+   
+console.log(big);  //  ->  1234567890000000000000n
+console.log(typeof  big);  //  ->  bigint
+   
+console.log(big2);  //  ->  1n
+console.log(7n  /  4n);  //  ->  1n
+
+
+//Number
+const  year  =  1991;
+let  delayInSeconds  =  0.00016;
+let  area  =  (16  *  3.14);
+let  halfArea  =  area  /  2;
+   
+console.log(year);  //  ->  1991;
+console.log(typeof  year);  //  ->  number;
+
+
